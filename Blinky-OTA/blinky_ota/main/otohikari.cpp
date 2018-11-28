c6474abe9b78f574a66233aca063c1b5983433dc
Robin Scheibler <fakufaku@gmail.com>
2018-11-29 08:49:22 +0900

    Add white LED mode

--- Blinky-OTA/blinky_ota/main/otohikari.cpp
+++ Blinky-OTA/blinky_ota/main/otohikari.cpp
@@ -99,7 +99,7 @@ float camera_pre_correction(float d, int n)
 
 // LED & AUDIO
 #if ENABLE_LOG
-#define MIN_DB (-80.0f)
+#define MIN_DB (-70.0f)
 #define MAX_DB (-10.0f)
 #endif
 
@@ -107,10 +107,10 @@ float camera_pre_correction(float d, int n)
 #define MAX_LIN 1e-3
 
 // States
-#define STATE_CALIBRATION 0
-#define STATE_RED_SIG_BLUE_REF 1
-#define STATE_HELLOWORLD 2
-#define STATE_3 3
+#define STATE_WHITE_SIG_NO_REF 0
+#define STATE_CALIBRATION 1
+#define STATE_RED_SIG_BLUE_REF 2
+#define STATE_HELLOWORLD 3
 #define STATE_4 4
 #define STATE_5 5
 #define STATE_6 6
@@ -173,6 +173,8 @@ void main_process()
 #if ENABLE_MONITOR
     uint32_t counter = 0;
 #endif
+
+    float elapsed_time;
 
     while (1) {
         
@@ -238,7 +240,7 @@ void main_process()
               ledC->updateDuty(leds[2], (1 << (LED_RESOLUTION - 1)) - 1);
               state_current = state_new;
             }
-        
+
             timer->start();
             // Only single channel processing
             for (int n=0; n<1; n++) {
@@ -284,12 +286,75 @@ void main_process()
 #endif
 
             }
-            float elapsed_time = timer->measure();
+            elapsed_time = timer->measure();
             if (elapsed_time > (float)AUDIO_BUFFER_SIZE/(float)SAMPLE_RATE*1000.0f) {
               // elapsed_time must be less than AUDIO_BUFFER_SIZE/SAMPLE_RATE*1000(msec)
               printf("elapsed_time must be less than %f(msec)\n", (float)AUDIO_BUFFER_SIZE/(float)SAMPLE_RATE*1000.0f);
             }
             break;
+
+          case STATE_WHITE_SIG_NO_REF:
+
+            if (state_new != state_current)
+            {
+              // Turn off all LEDs
+              for (int i=0 ; i < 4 ; i++)
+                ledC->updateDuty(leds[i], 0);
+              state_current = state_new;
+            }
+
+            timer->start();
+            // Only single channel processing
+            for (int n=0; n<1; n++) {
+              double amp_val = 0.;
+              double power_val = 0.;
+
+              for (int i=0; i<AUDIO_BUFFER_SIZE; i++) {
+                amp_val += audio_data[AUDIO_CHANNELS*i + n];
+                power_val += audio_data[AUDIO_CHANNELS*i + n] * audio_data[AUDIO_CHANNELS*i + n];
+              }
+
+              amp_val /= AUDIO_BUFFER_SIZE;
+
+              // This is the frame variance
+              double power_excluding_offset = power_val/AUDIO_BUFFER_SIZE - amp_val * amp_val;
+
+#if ENABLE_LOG
+              // Log of variance
+              float dB = 10.0f * log10f(power_excluding_offset);
+              float duty_f = (dB - MIN_DB) / (MAX_DB - MIN_DB);
+#else
+              // Linear from variance to PWM
+              float duty_f = (power_excluding_offset - MIN_LIN) / (MAX_LIN - MIN_LIN);
+#endif
+
+              if (duty_f < 0.0f) duty_f = 0.0f;
+              if (duty_f > 1.0f) duty_f = 1.0f;
+
+#if ENABLE_LOG
+              duty_f = map_pwm(duty_f);
+              duty_f = camera_pre_correction(duty_f, 2*n);
+#endif
+
+              uint32_t duty = (uint32_t)(duty_f*duty_max);
+              ledC->updateDuty(leds[1], duty);
+
+#if ENABLE_MONITOR
+              if (counter % 50 == 0)
+                printf("power_val=%e db=%e duty_f=%e duty=%d dip_val=%d\n",
+                    (double)power_excluding_offset, (double)dB, (double)duty_f, (int)duty, (int)dip_switch_read());
+
+              counter += 1;
+#endif
+
+            }
+            elapsed_time = timer->measure();
+            if (elapsed_time > (float)AUDIO_BUFFER_SIZE/(float)SAMPLE_RATE*1000.0f) {
+              // elapsed_time must be less than AUDIO_BUFFER_SIZE/SAMPLE_RATE*1000(msec)
+              printf("elapsed_time must be less than %f(msec)\n", (float)AUDIO_BUFFER_SIZE/(float)SAMPLE_RATE*1000.0f);
+            }
+            break;
+
         }
     }
 }

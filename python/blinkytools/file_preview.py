0600e1cfdb0572b89387645bd6af5161c612d3e2
Robin Scheibler <fakufaku@gmail.com>
2020-02-04 11:25:22 +0900

    Renamed the package to blinkytools. Added some scripts for easier use

--- python/blinkytools/file_preview.py
+++ python/blinkytools/file_preview.py
@@ -17,30 +17,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-import argparse
-from .gui import start_viewer
+from .io import file_preview
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="A video viewer to help setup and debug Blinkies"
-    )
-    parser.add_argument(
-        "-v",
-        "--video_source",
-        type=str,
-        default=0,
-        help="The video feed number (default 0)",
-    )
-    parser.add_argument(
-        "-i",
-        "--industrial",
-        action="store_true"
-    )
-    args = parser.parse_args()
-
-    try:
-        video_source = int(args.video_source)
-    except ValueError:
-        video_source = args.video_source
-
-    start_viewer(video_source, industrial=args.industrial)
+    file_preview()
--- python/blinkytools/gui.py
+++ python/blinkytools/gui.py
@@ -488,3 +488,34 @@ class BlinkyViewer(object):
 
 def start_viewer(video_source, industrial=False):
     BlinkyViewer(Tk(), video_source, industrial=industrial)
+
+def viewer_main():
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description="A video viewer to help setup and debug Blinkies"
+    )
+    parser.add_argument(
+        "-v",
+        "--video_source",
+        type=str,
+        default=0,
+        help="The video feed number (default 0)",
+    )
+    parser.add_argument(
+        "-i",
+        "--industrial",
+        action="store_true"
+    )
+    args = parser.parse_args()
+
+    try:
+        video_source = int(args.video_source)
+    except ValueError:
+        video_source = args.video_source
+
+    start_viewer(video_source, industrial=args.industrial)
+
+
+if __name__ == "__main__":
+    viewer_main()

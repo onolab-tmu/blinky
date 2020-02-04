7046049245b2b788e3ee5e3db110c560b2ee8f05
Robin Scheibler <fakufaku@gmail.com>
2020-02-04 10:31:57 +0900

    Moved python package to subdir, added license and README

---
+++ python/README.md
@@ -0,0 +1,4 @@
+Blinky Tools
+============
+
+This package contains a number of tools to work with Blinkies, small sound-to-light conversion sensors.
--- viewer/__init__.py
+++
@@ -1,5 +0,0 @@
-import tkinter
-
-from .gui import BlinkyViewer, start_viewer
-from .io import BlinkyFile
-from .non_linearity import blinky_non_linearity, blinky_non_linearity_inv, clip
